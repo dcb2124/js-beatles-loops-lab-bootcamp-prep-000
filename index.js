bfc7cd5b73c411e84f1ec2b670fde477abbbc86a
DAVID BILLINGSLEY <dcb2124@gmail.com>
2018-04-01 01:43:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function  theBeatlesPlay(musicians, instruments){
   
   var lineup = []
   
-  for (var i = 1, i < musicians.length, i++){
+  for (var i = 1; i < musicians.length; i++){
     
     lineup.push(`${musicians[i]} plays ${instruments[i]}`)
     
@@ -11,3 +11,16 @@ function  theBeatlesPlay(musicians, instruments){
   return lineup
   
 }
+
+function johnLennonFacts(facts){
+  
+  var i = 0
+  
+  while (i < facts.length){
+    facts = facts + "!!!"
+  }
+  
+  return facts
+}
+
+function
